7f0734a2eacbaeae3203c81b7c0842dfb037b17c
Gulbahar-zengin <zengingulbahar2304@gmail.com>
2025-07-16 11:51:06 +0300

    frame e göre interval eklendi ve yolo bboxları içinden ilki değil de en yksek conf olan seçildi

--- boşluk_dold.py
+++ boşluk_dold.py
@@ -14,8 +14,8 @@ def calculate_intersection_over_union(box1, box2):
     xB = min(x1_2, x2_2)
     yB = min(y1_2, y2_2)
 
-    intersection_width = max(0, xB - xA + 1)
-    intersection_height = max(0, yB - yA + 1)
+    intersection_width = max(0, xB - xA )
+    intersection_height = max(0, yB - yA)
     intersection_area = intersection_width * intersection_height
     box1_area = w1 * h1
     box2_area = w2 * h2
@@ -24,16 +24,18 @@ def calculate_intersection_over_union(box1, box2):
     return iou
 
 
-model = YOLO("best_h.pt", verbose=False).to("cuda:0")
+model = YOLO(r"C:\Users\Zengin\Desktop\Gökbörü\2024-25 Savaşan iha\takip_goruntu isleme\best_h.pt", verbose=False).to("cuda:0")
 ffmpeg="udp://127.0.0.1:5001"
-cap = cv2.VideoCapture(ffmpeg)
+cap = cv2.VideoCapture(0)
 
 tracker = None
 tracking = False
-matching_threshold = 0.3  #ıou eşiği
+matching_threshold = 0.1  #ıou eşiği
 last_yolo_time = time.time()
-yolo_interval = 1.0  # YOLO kaç saniyede bir çalıştırılsın
-
+# yolo_interval = 1.0   #YOLO kaç saniyede bir çalıştırılsın
+yolo_interval_frame=30
+frame_counter=0
+frame_fps=0
 
 tracker_start_time = None
 start_time = time.time()
@@ -42,6 +44,7 @@ bbox_tracker = None
 
 while True:
     ret, frame = cap.read()
+    frame_fps +=1
     if not ret:
         break
 
@@ -52,29 +55,35 @@ while True:
             x, y, w, h = map(int, bbox_tracker)
             elapsed_time = time.time() - tracker_start_time
             elapsed_text = f"{elapsed_time:.1f} sec"
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(frame, elapsed_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 1)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            cv2.putText(frame, elapsed_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
         else:
             tracking = False
             last_yolo_time= 0
-
+    frame_counter +=1
+    """ Bu kısım saniye cinsinden çalıştırmak için
     # YOLO belirlenen aralıkta çalışır
     current_time = time.time()
     if current_time - last_yolo_time >= yolo_interval:
-        results = model(source=frame, conf=0.4, verbose=False)
-        last_yolo_time = current_time
+    """
+    if frame_counter >= yolo_interval_frame:
+        results = model.predict(source=frame, conf=0.4, verbose=False)
+        frame_counter = 0 
+    #     last_yolo_time = current_time
 
         boxes = results[0].boxes.xyxy.cpu().numpy() if results and results[0].boxes.xyxy is not None else []
 
         if len(boxes) > 0:
-            x1, y1, x2, y2 = boxes[0]
+            confidences= results[0].boxes.conf.cpu().numpy()
+            best_id= confidences.argmax()
+            x1, y1, x2, y2 = boxes[best_id]
             bbox_yolo = (x1, y1, x2 - x1, y2 - y1)
             x, y, w, h = map(int, bbox_yolo)
 
             iou = 0
             if bbox_tracker is not None:
                 iou = calculate_intersection_over_union(bbox_yolo, bbox_tracker)
-                print(f"IoU: {iou:.2f}")
+                # print(f"IoU: {iou:.2f}")
 
             # IoU düşükse veya takip yoksa yeni takip başlatılır
             if (iou < matching_threshold) or not tracking:
@@ -91,10 +100,14 @@ while True:
     gecen_sure = time.time() - start_time
     fps_adjust = max(0, (1 / 15) - gecen_sure)
     time.sleep(fps_adjust)
-    now = datetime.datetime.now()
-    timestamp = now.strftime("%M:%S.") + f"{int(now.microsecond / 1000):03d}"
-    print(f"[{timestamp}] FPS: {round(1 / (time.time() - start_time))}")
-    start_time = time.time()
+    # now = datetime.datetime.now()
+    # timestamp = now.strftime("%M:%S.") + f"{int(now.microsecond / 1000):03d}"
+    # print(f"[{timestamp}] FPS: {round(1 / (time.time() - start_time))}")
+    if gecen_sure > 1.0:
+        avg= gecen_sure/frame_fps
+        print("fps:",frame_fps/gecen_sure)
+        frame_fps=0
+        start_time = time.time()
 
     cv2.imshow("YOLO + Tracker", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
